5bd40a170e546167f6f695742e22929ea3322acc
ahdan86 <ahdanamanullah@gmail.com>
2021-10-10 16:12:01 +0700

    Update

--- tugas-threejs/main.js
+++ tugas-threejs/main.js
@@ -1,27 +1,10 @@
 import * as THREE from "./node_modules/three/src/Three.js";
 import { OrbitControls } from "./node_modules/three/examples/jsm/controls/OrbitControls.js";
+import { GUI } from "./node_modules/dat.gui/build/dat.gui.module.js";
 
-let scene, camera, renderer, controls;
+let scene, camera, renderer, controls, canvas, cube, gui;
 
-// set up the environment - // initiallize scene, camera, objects and renderer
-let init = function () {
-  // 1. create the scene
-  scene = new THREE.Scene();
-  scene.background = new THREE.Color(0xefefef);
-
-  // 2. create an locate the camera + add control orbit for move around the camera
-  const fov = 45;
-  const aspect = 2; // the canvas default
-  const near = 0.1;
-  const far = 100;
-  camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-  camera.position.set(0, 10, 20);
-
-  controls = new OrbitControls(camera, canvas);
-  controls.target.set(0, 5, 0);
-  controls.update();
-
-  // 3. Make a Plane
+let createPlane = function () {
   const planeSize = 40;
   const loader = new THREE.TextureLoader();
   const texture = loader.load("checker.png");
@@ -36,19 +19,75 @@ let init = function () {
     map: texture,
     side: THREE.DoubleSide,
   });
-  const mesh = new THREE.Mesh(planeGeo, planeMat);
-  mesh.rotation.x = Math.PI * -0.5;
-  scene.add(mesh);
+  const plane = new THREE.Mesh(planeGeo, planeMat);
+  plane.rotation.x = Math.PI * -0.5;
+  scene.add(plane);
+};
 
-  // 4. create the renderer
-  var canvReference = document.getElementById("myCanvas");
+let createCube = function () {
+  const cubeSize = 4;
+  const cubeGeo = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize);
+  const cubeMat = new THREE.MeshPhongMaterial({ color: "#8AC" });
+  cube = new THREE.Mesh(cubeGeo, cubeMat);
+  cube.position.set(0, 5, 0);
+  scene.add(cube);
+};
+
+// set up the environment - // initiallize scene, camera, objects and renderer
+let init = function () {
+  // 1. create the scene
+  canvas = document.getElementById("myCanvas");
   renderer = new THREE.WebGLRenderer({
     antialias: true,
-    canvas: canvReference,
+    canvas: canvas,
   });
-  renderer.setSize(750, 750);
+
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color(0x000000);
+
+  // 2. create an locate the camera + add control orbit for move around the camera
+  const fov = 45;
+  const aspect = 2; // the canvas default
+  const near = 0.1;
+  const far = 100;
+  camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  camera.position.set(0, 10, 20);
+
+  controls = new OrbitControls(camera, canvas);
+  controls.target.set(0, 5, 0);
+  controls.update();
+
+  // 3. Create Lighting and GUI for Lighting
+  const color = 0xffffff;
+  const intensity = 1;
+  const light = new THREE.AmbientLight(color, intensity);
+  scene.add(light);
+
+  const gui = new GUI();
+  gui.addColor(new ColorGUIHelper(light, "color"), "value").name("color");
+  gui.add(light, "intensity", 0, 2, 0.01);
+
+  // 4. Make a Plane and Shapes
+  createPlane();
+  createCube();
+
+  // 5. Create the Renderer
+  renderer.setSize(750, 550);
   document.body.appendChild(renderer.domElement);
 };
+
+class ColorGUIHelper {
+  constructor(object, prop) {
+    this.object = object;
+    this.prop = prop;
+  }
+  get value() {
+    return `#${this.object[this.prop].getHexString()}`;
+  }
+  set value(hexString) {
+    this.object[this.prop].set(hexString);
+  }
+}
 
 // main animation loop - calls 50-60 in a second.
 let speed = 0.01;
